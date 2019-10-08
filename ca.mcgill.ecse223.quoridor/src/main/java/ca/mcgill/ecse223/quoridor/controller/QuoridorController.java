5323922b88375416b343de5e913119f5f62bf9e7
Thomas Philippon <thomas.philippon@mail.mcgill.ca>
2019-10-08 16:00:00 -0400

    Create the controller package

---
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse223.quoridor.controller;
+
+import ca.mcgill.ecse223.quoridor.QuoridorApplication;
+import ca.mcgill.ecse223.quoridor.model.*;
+
+public class QuoridorController {
+	
+
+
+	
+
+	
+}
