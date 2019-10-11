d085d5882f54c5a30275687534f48b1f6ce469fd
David Deng <zhiyun.deng@mail.mcgill.ca>
2019-10-11 18:08:16 -0400

    David's features are done

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
@@ -30,7 +30,9 @@ public class QuoridorController {
 	/**
 	 * get the wallMove object associated with the current game and current player with the required orientation, row, and column.
 	 * If no such wallMove object exist, (i.e. there is no such a wall with such a position and orientation), a new object will be created
-	 * and linkedin added to model. 
+	 * and links added to model. (feature:move wall) 
+	 * 
+	 * @author David
 	 * @param dir orientation of the wall "vertical","horizontal"
 	 * @param row row number of tile northwest of centrepoint of wall
 	 * @param column column number
@@ -39,8 +41,9 @@ public class QuoridorController {
 	public static void getWallMove(String dir, int row, int column) throws Throwable{
 		throw new java.lang.UnsupportedOperationException();
 	}
-	/**
-	 *  
+	/**(feature: move wall)
+	 * 
+	 * @author David 
 	 * @param side the side to check for edges. "left", "right", "up", "down"
 	 * @throws Throwable
 	 * @return true if the current wall selected is at the edge specified in the parameter
@@ -50,7 +53,8 @@ public class QuoridorController {
 	}
 	/**
 	 *  moves the wall one tile toward the direction specified. An illegal move notification will be shown
-	 *  if such a move in illegal
+	 *  if such a move in illegal. This involves linking wallMove object to a new target tile(feature: move wall)
+	 *  @author David
 	 * @param side the direction at which we move the wall. "left", "right", "up", "down"
 	 * @throws Throwable
 	 * 
@@ -60,7 +64,8 @@ public class QuoridorController {
 	}
 	/**
 	 *  Each player is given a fixed time limit for a game. This method changes the remaining thinking
-	 *  time of each player
+	 *  time of each player (feature: set total thinking time)
+	 * @author David
 	 * @param min the minute part of the time
 	 * @param second the second part of the time
 	 * @throws Throwable
@@ -70,7 +75,24 @@ public class QuoridorController {
 		throw new java.lang.UnsupportedOperationException();
 	}
 	/**
-	 *  Communicates to View to check if a Illegal Move Notification is being displayed
+	 *  Optional feature: Set thinking time for a specific player. 
+	 *  We know that both players are supposed to have the same thinking time. 
+	 *  However, being able to set time for individual players can to increase the difficulty for a more experienced player without affecting the other opponent. 
+	 *  Each player is given a fixed time limit for a game. This method changes the remaining thinking
+	 *  time of each player 
+	 *  @author David
+	 * @param min the minute part of the time
+	 * @param second the second part of the time
+	 * @param playerIndex specifies for whom this thinking time is for
+	 * @throws Throwable
+	 * 
+	 */
+	public static void setThinkingTime(int min, int sec, int playerIndex) {
+		
+	}
+	/**
+	 *  Communicates to View to check if a Illegal Move Notification is being displayed (feature: move wall)
+	 *  @author David
 	 * @throws Throwable
 	 * @return true if notification displayed, false otherwise
 	 * 
@@ -78,7 +100,16 @@ public class QuoridorController {
 	public static boolean isIllegalMoveNotificationDisplayed() {
 		throw new java.lang.UnsupportedOperationException();
 	}
-	
+	/**
+	 *  Communicates to View to check if a wall is currently is being displayed at a certain location (feature: move wall)
+	 *  @author David
+	 * @throws Throwable
+	 * @return true if wall displayed at specified location, false otherwise
+	 * 
+	 */
+	public static boolean thisWallIsAtPosition(int row, int column) {
+		throw new java.lang.UnsupportedOperationException();
+	}
 	
 	
 
