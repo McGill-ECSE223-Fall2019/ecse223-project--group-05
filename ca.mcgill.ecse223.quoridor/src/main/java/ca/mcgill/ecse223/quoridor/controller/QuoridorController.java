e439562a58f43cadde653397d1e3905fb9e3ae25
David Deng <zhiyun.deng@mail.mcgill.ca>
2019-10-11 18:08:16 -0400

    David's features are done

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
@@ -128,6 +128,15 @@ public class QuoridorController {
 	public static Boolean grabWall(Game game) {
 		
 		throw new java.lang.UnsupportedOperationException("This controller method is not implemented yet");
+	/**
+	 *  Communicates to View to check if a wall is currently is being displayed at a certain location (feature: move wall)
+	 *  @author David
+	 * @throws Throwable
+	 * @return true if wall displayed at specified location, false otherwise
+	 * 
+	 */
+	public static boolean thisWallIsAtPosition(int row, int column) {
+		throw new java.lang.UnsupportedOperationException();
 	}
 	
 	/**
@@ -238,42 +247,6 @@ public class QuoridorController {
 		throw new java.lang.UnsupportedOperationException("This controller method is not implemented yet");
 	}
 	
-    /**
-     * Method to release current Wall in my hand and register move
-     * @param game Current Game Object, to retrieve the wallMoveCandidate in the game and any other necessary info
-     * @return True if releaseWall was successful and wall could be registered, false if not
-     * @author Alex Masciotra
-     */
-    public static Boolean releaseWall(Game game){
-        throw new java.lang.UnsupportedOperationException("Not yet implemented");
-    }
-
-    /***
-     * Method to set existing username to player
-     * @param userName username to set to player
-     * @param game Quoridor Game in order to get the player and set username to that existing player
-     * @return True if successful, false if not
-     * @author Alex Masciotra
-     */
-    public static Boolean selectExistingUserName(String userName, Game game){
-
-        throw new java.lang.UnsupportedOperationException("Not yet implemented");
-
-
-        //get all existing usernames and see if username provided is found
-    }
-
-    /**
-     * Method to assign new Username to a player
-     * @param userName username to set to player
-     * @param game Quoridor Game Application in order to get the player and set username to that player
-     * @author Alex Masciotra
-     * @return True assigning of new username was correct / False if name already exists
-     */
-    public static Boolean selectNewUserName(String userName, Game game){ //could be boolean to see if it could indeed set
-
-        throw new java.lang.UnsupportedOperationException("Not yet implemented");
-
     /**
      * Method to release current Wall in my hand and register move
      * @param game Current Game Object, to retrieve the wallMoveCandidate in the game and any other necessary info
