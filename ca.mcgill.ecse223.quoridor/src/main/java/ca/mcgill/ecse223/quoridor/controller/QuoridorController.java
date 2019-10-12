fafaa72a97c1eee005f122dc39c2aaa674f70b45
Matthias Arabian <matthias15002@gmail.com>
2019-10-11 20:12:29 -0400

    Draft2 of Matthias' independent features for deliverable2

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
@@ -29,4 +29,8 @@ public class QuoridorController {
 		throw new UnsupportedOperationException("Failed to set player position");
 		
 	}
+
+	public static Boolean sendLoadError() throws UnsupportedOperationException{
+		throw new UnsupportedOperationException("sendLoadError");
+	}
 }
