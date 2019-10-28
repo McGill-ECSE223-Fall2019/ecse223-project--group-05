515cde2523b070524b34f37d95ec7c84f0db8c71
amasciotra <alexmasciotra@gmail.com>
2019-10-28 18:58:00 -0400

    WIP, Username complete, dropWall WIP
    related to issue #22, #27

--- src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
+++ src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.net.UnknownServiceException;
+import java.util.*;
 import java.io.IOException;
 
 import ca.mcgill.ecse223.quoridor.QuoridorApplication;
