df21e58204d9ceae11f224609d883002691e58bb
Matthias Arabian <matthias15002@gmail.com>
2019-10-30 00:19:34 -0400

    First commit. Put GUI prototype onto GitHub

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,54 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.transform.Scale;
+
+
+public class Main extends Application {
+	//scaling variables (for resizing of window)
+	private double initialH, initialW;
+	
+	@Override
+	public void start(Stage primaryStage) {
+        //load fxml file and display it in the stage
+		try {
+	        FXMLLoader loader = new FXMLLoader(getClass().getResource("advs.fxml"));
+	        Parent root = loader.load();
+	        Scene scene = new Scene(root);
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+	        
+	        //initialize constants
+	        initialH = scene.getHeight();
+	        initialW = scene.getWidth();
+	        
+	        
+	        //set event listener for resizing
+	        scene.widthProperty().addListener((obs, oldVal, newVal) -> {
+	        	Scale scale = new Scale(newVal.doubleValue()/initialW, scene.getHeight()/initialH);
+	        	scale.setPivotX(0);
+	        	scale.setPivotY(0);
+	        	scene.getRoot().getTransforms().setAll(scale);
+	       });
+	
+	       scene.heightProperty().addListener((obs, oldVal, newVal) -> {
+	       	Scale scale = new Scale(scene.getWidth()/initialW, newVal.doubleValue()/initialH);
+	       	scale.setPivotX(0);
+	       	scale.setPivotY(0);
+	       	scene.getRoot().getTransforms().setAll(scale);
+	       });
+        
+		} catch (IOException e) {e.printStackTrace();}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/main/java/application/ViewInterface.java
@@ -0,0 +1,170 @@
+//THE IMPORT STATEMENT BELOW IS REQUIRED IN THE .fxml FILE
+//IT GETS DELETED WHEN YOU REGENERATE THE .fxml FILE WITH SCENE BUILDER
+//YOU MUST ADD IT IN MANUALLY WHEN THAT HAPPENS
+
+//<?import java.util.ArrayList?>
+
+
+package application;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class ViewInterface {
+	
+	//these are the pages that the user can travel to/interact with
+	private enum Page {
+		MAIN_PAGE,
+		NEW_GAME_PAGE,
+		TOP_BUTTONS,
+		RULES_PAGE;
+		
+	};
+	
+	//this list is initialized in the fxml file and is populated with 
+	//GUI pages that the user can travel to/interact with.
+//////ORDER MATTERS. IF YOU ADD PAGES IN THE FXML PAGE, MAKE SURE THAT	////////
+//////YOU UPDATE THE ENUM AND THE getPage(Page Cur) FUNCTION			////////
+	@FXML
+	private List<AnchorPane> pageList;
+
+	//generalize events by storing current page as a page/state pair
+	private Page currentState = Page.MAIN_PAGE;
+	AnchorPane CurrentPage = null;
+	
+	/**
+	 * @author Matthias Arabian
+	 * Changes the GUI CurrentPage to the Create New Game Page.
+	 * Disables and turns the past page invisible.
+	 */
+	public void Goto_New_Game_Page() {
+		CurrentPage = getCurrentPage();
+		
+//		Restrict page to page movement when the current page is disabled 
+//		(something else is happening atm)
+		if (CurrentPageIsDisabled())
+			return;
+		
+		CurrentPage.setDisable(true);
+		CurrentPage.setVisible(false);
+		
+		currentState = Page.NEW_GAME_PAGE;
+		CurrentPage = getCurrentPage();
+		CurrentPage.setDisable(false);
+		CurrentPage.setVisible(true);
+		CurrentPage.toFront();
+	}
+	
+	/**
+	 * @author Matthias Arabian
+	 * Changes the GUI CurrentPage to the Main Page.
+	 * Disables and turns the past page invisible.
+	 */
+	public void Goto_Main_Page() {
+		CurrentPage = getCurrentPage();
+		
+//		Restrict page to page movement when the current page is disabled 
+//		(something else is happening atm)
+		if (CurrentPageIsDisabled())
+			return;
+			
+		CurrentPage.setDisable(true);
+		CurrentPage.setVisible(false);
+
+		currentState = Page.MAIN_PAGE;
+		CurrentPage = getCurrentPage();
+		CurrentPage.setDisable(false);
+		CurrentPage.setVisible(true);
+		CurrentPage.toFront();
+	}
+	
+	/**
+	 * @author Matthias Arabian
+	 * Displays the Rules page. 
+	 * Disables the CurrentPane, but does not replace it.
+	 * Overlays the rules on top of the CurrentPage to allow user to return to what
+	 * they were doing.
+	 */
+	public void openRulesPage() {
+		CurrentPage = getCurrentPage();
+		
+		//if page is disabled, then the rules are already displayed. Therefore, the rules page should be closed.
+		if (CurrentPageIsDisabled()) { 
+			closeRulesPage();
+			return;
+		}
+		
+		
+		//disable the current page, but do not make it invisible or replace it.
+		CurrentPage.setDisable(true);
+		
+		//display the Rules page, and bring it to front to allow for user interaction.
+		CurrentPage = getPage(Page.RULES_PAGE);
+		CurrentPage.setDisable(false);
+		CurrentPage.setVisible(true);
+		CurrentPage.toFront();
+		getPage(Page.TOP_BUTTONS).toFront(); //those need to be on top of the rules to allow for clicking on Rules label to close rules
+		
+	}
+	
+	/**
+	 * @author Matthias Arabian
+	 * this function closes the Rules page and re-enables the CurrentPage.
+	 */
+	public void closeRulesPage() {
+		CurrentPage = getPage(Page.RULES_PAGE);
+		CurrentPage.setDisable(true);
+		CurrentPage.setVisible(false);
+		CurrentPage = getCurrentPage();
+		CurrentPage.setDisable(false);
+		CurrentPage.toFront();
+	}
+	
+	/**
+	 * @author Matthias Arabian
+	 * @return whether CurrentPage is disabled
+	 */
+	private boolean CurrentPageIsDisabled() {
+		return getPage(currentState).isDisable();
+	}
+	
+	/**
+	 * @author Matthias Arabian
+	 * @return the current page the user is interacting with
+	 */
+	private AnchorPane getCurrentPage() {
+		return getPage(currentState);
+	}
+	
+	/**
+	 * @author Matthias Arabian
+	 * @param Cur: an enum type used to discriminate between GUI pages in a user-friendly manner.
+	 * @return the page described by the variable Cur
+	 * 
+	 * The order of the pages stored in the pageList variable depends on 
+	 * their order when the list was declared in the FXML file.
+	 * You must ensure that the pages referenced in this function are the intended ones.
+	 * 
+	 * This is a necessary evil that allows for greater generalization in event handlers.
+	 */
+	private AnchorPane getPage(Page Cur) {
+		if (Cur == Page.NEW_GAME_PAGE)
+			return pageList.get(1);
+		if (Cur == Page.MAIN_PAGE)
+			return pageList.get(0);
+		if (Cur == Page.RULES_PAGE)
+			return pageList.get(3);
+		if (Cur == Page.TOP_BUTTONS)
+			return pageList.get(2);
+		else 
+			return null;
+	}
+}
