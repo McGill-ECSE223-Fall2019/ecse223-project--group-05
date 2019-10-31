2bec39ca296ec810567836fbf95ffb627adb0a60
Matthias Arabian <matthias15002@gmail.com>
2019-10-31 19:55:49 -0400

    GUI 90% functional. Can begin implementing contoller functions

--- src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
+++ src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
@@ -7,18 +7,21 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
+import application.ViewInterface;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.transform.Scale;
 
 
 public class QuoridorApplication extends Application{
 
 	private static Quoridor quoridor;
-
+	private static ViewInterface c;
+	
 	//scaling variables (for resizing of window)
 	private double initialH, initialW;
 
@@ -30,32 +33,36 @@ public class QuoridorApplication extends Application{
 			System.out.println(resourceFile.toString());
 			URL url = resourceFile.toURI().toURL();
 			FXMLLoader loader = new FXMLLoader(url);
-			//URL url = new URL("file:///view/advs.fxml");
-			//loader.setLocation(url);
 			Parent root = loader.load();
-			Scene scene = new Scene(root);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-
-			//initialize constants
-			initialH = scene.getHeight();
-			initialW = scene.getWidth();
-
-
-			//set event listener for resizing
-			scene.widthProperty().addListener((obs, oldVal, newVal) -> {
-				Scale scale = new Scale(newVal.doubleValue()/initialW, scene.getHeight()/initialH);
-				scale.setPivotX(0);
-				scale.setPivotY(0);
-				scene.getRoot().getTransforms().setAll(scale);
-			});
-
-			scene.heightProperty().addListener((obs, oldVal, newVal) -> {
-				Scale scale = new Scale(scene.getWidth()/initialW, newVal.doubleValue()/initialH);
-				scale.setPivotX(0);
-				scale.setPivotY(0);
-				scene.getRoot().getTransforms().setAll(scale);
-			});
+			c = loader.getController(); //get reference to viewInterface
+	        Scene scene = new Scene(root);  
+	        primaryStage.setScene(scene);
+	        
+	        //set the icon and title of window
+	        primaryStage.getIcons().add(new Image("src/main/resources/logo_icon.png"));
+	        primaryStage.setTitle("Quoridor game. Group 5");
+	        
+	        primaryStage.show();
+	        
+	        //initialize constants
+	        initialH = scene.getHeight();
+	        initialW = scene.getWidth();
+	        
+	        
+	        //set event listener for resizing
+	        scene.widthProperty().addListener((obs, oldVal, newVal) -> {
+	        	Scale scale = new Scale(newVal.doubleValue()/initialW, scene.getHeight()/initialH);
+	        	scale.setPivotX(0);
+	        	scale.setPivotY(0);
+	        	scene.getRoot().getTransforms().setAll(scale);
+	       });
+	
+	        scene.heightProperty().addListener((obs, oldVal, newVal) -> {
+	       	Scale scale = new Scale(scene.getWidth()/initialW, newVal.doubleValue()/initialH);
+	       	scale.setPivotX(0);
+	       	scale.setPivotY(0);
+	       	scene.getRoot().getTransforms().setAll(scale);
+	       });
 
 		} catch (IOException e) {e.printStackTrace();}
 	}
@@ -66,6 +73,10 @@ public class QuoridorApplication extends Application{
 			quoridor = new Quoridor();
 		}
  		return quoridor;
+	}
+	
+	public static ViewInterface getViewInterface() {
+		return c;
 	}
 
 	public static void main(String[] args) {
--- src/main/java/ca/mcgill/ecse223/quoridor/view/ViewInterface.java
+++ src/main/java/ca/mcgill/ecse223/quoridor/view/ViewInterface.java
@@ -9,30 +9,86 @@ package ca.mcgill.ecse223.quoridor.view;
 
 import java.util.List;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
+import javafx.collections.FXCollections;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
 public class ViewInterface {
 	
 	//these are the pages that the user can travel to/interact with
 	private enum Page {
+		TOP_BUTTONS,
 		MAIN_PAGE,
 		NEW_GAME_PAGE,
-		TOP_BUTTONS,
-		RULES_PAGE;
-		
+		RULES_PAGE,
+		LOAD_GAME_PAGE,
+		SELECT_HOST_PAGE,
+		GAME_SESSION_PAGE,
+		CHOOSE_OPPONENT_PAGE;
 	};
 	
 	//this list is initialized in the fxml file and is populated with 
 	//GUI pages that the user can travel to/interact with.
 //////ORDER MATTERS. IF YOU ADD PAGES IN THE FXML PAGE, MAKE SURE THAT	////////
 //////YOU UPDATE THE ENUM AND THE getPage(Page Cur) FUNCTION			////////
-	@FXML
-	private List<AnchorPane> pageList;
-
+	@FXML private List<AnchorPane> pageList;
 	//generalize events by storing current page as a page/state pair
 	private Page currentState = Page.MAIN_PAGE;
 	AnchorPane CurrentPage = null;
+
+	@FXML private ComboBox ComboBox_username1;
+	@FXML private ComboBox ComboBox_username2;
+	
+	@FXML ListView loadedGameList;
+	private int counter = 0;
+	
+	
+	
+//Load Game Page
+	@FXML private Label lbl_directory;
+	
+	
+	
+	public void addToLoadedGameList() {
+            	Stage stage = new Stage();
+            	DirectoryChooser fileChooser = new DirectoryChooser();
+            	fileChooser.setTitle("Open Resource File");
+            	File file = fileChooser.showDialog(stage);
+            	stage.close();
+                if (file != null) {
+                    lbl_directory.setText(file.toString());
+                    detectGameFiles(file);
+                }
+                else {
+                	lbl_directory.setText("Directory could not be opened");
+                }
+	}
+	
+	private void detectGameFiles(File file) {
+		File[] gameFiles = file.listFiles(new FilenameFilter() {
+		    public boolean accept(File dir, String name) {
+		    	return name.endsWith("txt");
+		        //return name.startsWith("temp") && name.endsWith("txt");
+		    }
+		});
+		System.out.println(gameFiles);
+		loadedGameList.getItems().clear();
+		for (File f : gameFiles)
+			loadedGameList.getItems().add(f.toString());
+	}
+
+	public void gotSelected() {
+		System.out.println(loadedGameList.getSelectionModel().getSelectedItem());
+	}
 	
 	/**
 	 * @author Matthias Arabian
@@ -40,21 +96,7 @@ public class ViewInterface {
 	 * Disables and turns the past page invisible.
 	 */
 	public void Goto_New_Game_Page() {
-		CurrentPage = getCurrentPage();
-		
-//		Restrict page to page movement when the current page is disabled 
-//		(something else is happening atm)
-		if (CurrentPageIsDisabled())
-			return;
-		
-		CurrentPage.setDisable(true);
-		CurrentPage.setVisible(false);
-		
-		currentState = Page.NEW_GAME_PAGE;
-		CurrentPage = getCurrentPage();
-		CurrentPage.setDisable(false);
-		CurrentPage.setVisible(true);
-		CurrentPage.toFront();
+		Goto_Page(Page.NEW_GAME_PAGE);
 	}
 	
 	/**
@@ -63,6 +105,53 @@ public class ViewInterface {
 	 * Disables and turns the past page invisible.
 	 */
 	public void Goto_Main_Page() {
+		Goto_Page(Page.MAIN_PAGE);
+	}
+	
+	/**
+	 * @author Matthias Arabian
+	 * Changes the GUI CurrentPage to the Choose Opponent Page.
+	 * Disables and turns the past page invisible.
+	 */
+	public void Goto_Choose_Opponent_Page() {
+		Goto_Page(Page.CHOOSE_OPPONENT_PAGE);
+	}
+	
+	/**
+	 * @author Matthias Arabian
+	 * Changes the GUI CurrentPage to the Game Session Page.
+	 * Disables and turns the past page invisible.
+	 */
+	public void Goto_Game_Session_Page() {
+		Goto_Page(Page.GAME_SESSION_PAGE);
+	}
+	
+	/**
+	 * @author Matthias Arabian
+	 * Changes the GUI CurrentPage to the Load Game Page.
+	 * Disables and turns the past page invisible.
+	 */
+	public void Goto_Load_Game_Page() {
+		Goto_Page(Page.LOAD_GAME_PAGE);
+	}
+	
+	/**
+	 * @author Matthias Arabian
+	 * Changes the GUI CurrentPage to the Select Host Page.
+	 * Disables and turns the past page invisible.
+	 */
+	public void Goto_Select_Host_Page() {
+		Goto_Page(Page.SELECT_HOST_PAGE);
+	}
+	
+	/**
+	 * @author Matthias Arabian
+	 * @param p The page to go to 
+	 * Changes the GUI CurrentPage to the page defined by the parameter p
+	 * This function is called by every other Goto_ functions
+	 * and serves as a generalized template for page travel.
+	 */
+	private void Goto_Page(Page p) {
 		CurrentPage = getCurrentPage();
 		
 //		Restrict page to page movement when the current page is disabled 
@@ -73,7 +162,7 @@ public class ViewInterface {
 		CurrentPage.setDisable(true);
 		CurrentPage.setVisible(false);
 
-		currentState = Page.MAIN_PAGE;
+		currentState = p;
 		CurrentPage = getCurrentPage();
 		CurrentPage.setDisable(false);
 		CurrentPage.setVisible(true);
@@ -150,15 +239,35 @@ public class ViewInterface {
 	 * This is a necessary evil that allows for greater generalization in event handlers.
 	 */
 	private AnchorPane getPage(Page Cur) {
-		if (Cur == Page.NEW_GAME_PAGE)
-			return pageList.get(1);
-		if (Cur == Page.MAIN_PAGE)
+		if (Cur == Page.TOP_BUTTONS)
 			return pageList.get(0);
+		if (Cur == Page.MAIN_PAGE)
+			return pageList.get(1);
+		if (Cur == Page.NEW_GAME_PAGE)
+			return pageList.get(2);
 		if (Cur == Page.RULES_PAGE)
 			return pageList.get(3);
-		if (Cur == Page.TOP_BUTTONS)
-			return pageList.get(2);
+		if (Cur == Page.LOAD_GAME_PAGE)
+			return pageList.get(4);
+		if (Cur == Page.SELECT_HOST_PAGE)
+			return pageList.get(5);
+		if (Cur == Page.GAME_SESSION_PAGE)
+			return pageList.get(6);
+		if (Cur == Page.CHOOSE_OPPONENT_PAGE)
+			return pageList.get(7);
 		else 
 			return null;
+	}
+	
+	
+	/**
+	 * @author Matthias Arabian
+	 * initializes the FXML components. this code runs once the application is launched but before the GUI is displayed.
+	 */
+	public void initialize() {
+		ComboBox_username1.setItems(FXCollections.observableArrayList(
+			    "A", "B", "C", "D"));
+		ComboBox_username2.setItems(FXCollections.observableArrayList(
+			    "A", "B", "C", "D"));
 	}
 }
