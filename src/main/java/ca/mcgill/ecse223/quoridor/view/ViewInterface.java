0936b010332af9f441bbba68038e9d97bc38953e
Matthias Arabian <matthias15002@gmail.com>
2019-11-28 12:22:46 -0500

    disabled tutorial for now. Thinking about deleting that feature completely

--- src/main/java/ca/mcgill/ecse223/quoridor/view/ViewInterface.java
+++ src/main/java/ca/mcgill/ecse223/quoridor/view/ViewInterface.java
@@ -2016,7 +2016,8 @@ public class ViewInterface {
      * @author Matthias Arabian
      */
     public void Start_Tutorial(){
-        isInTutorial = true;
+        isInTutorial = false;
+        System.out.println("Tutorial is disabled");
     }
     /**
      * @author Matthias Arabian
